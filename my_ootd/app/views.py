05bb6881d4dd2f60cf30cc2fa9c56a0e6d778c6c
dbckd999 <dbckd999@naver.com>
2023-03-29 03:11:28 +0900

    메인페이지 파일 작성

--- my_ootd/app/views.py
+++ my_ootd/app/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello world")
+
+
+def welcome(request):
+    template = loader.get_template('app/welcome.html')
+    context = {}
+    # return HttpResponse(template.render(request))
+    return render(request, 'app/welcome.html')
