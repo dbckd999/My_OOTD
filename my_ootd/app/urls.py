ee53112f5c6a7184adf8c2153ad3073368fc0f21
dbckd999 <dbckd999@naver.com>
2023-04-08 18:43:48 +0900

    회원관련 페이지 생성

--- my_ootd/app/urls.py
+++ my_ootd/app/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'app'
+
 urlpatterns = [
-    path('', views.index),
-    path('welcome/', views.welcome, name='welcome'),
-    path('welcome/sign-up', views.sign_up, name='sign_up'),
-    path('welcome/sign-up-proc', views.sign_up_proc, name='sign_up_proc'),
+    # path('', views.welcome, name='welcome'),
+    path('', views.root, name='root'),
+    # 회원가입 페이지
+    path('sign-up', views.sign_up, name='sign_up'),
+    # 회원가입 데이터 전송
+    path('sign_up_proc', views.sign_up_proc, name='sign_up_proc'),
+    path('login', views.get_login_page, name='login'),
 ]
