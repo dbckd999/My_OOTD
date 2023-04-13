2ff19a6eb32f4251afd2016217002e1daf4aa6c1
dbckd999 <dbckd999@naver.com>
2023-04-14 06:07:34 +0900

    회원가입 폼 클래스 추가

--- my_ootd/app/forms.py
+++ my_ootd/app/forms.py
@@ -1,7 +1,25 @@
 from django import forms
-from .models import UserClothes
+from .models import UserClothes, SevUser
+from django.contrib.auth.forms import UserCreationForm
+
 
 class UserClothesForm(forms.ModelForm):
     class Meta:
         model = UserClothes
         fields = ['user_id', 'username', 'cloth_name', 'cloth_var', 'cloth_col_1', 'cloth_col_2']
+
+
+# 회원가입 폼
+class SevUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = SevUser
+        fields = (
+            'username'
+            , 'password1'
+            , 'password2'
+            , 'email'
+            , 'nickname'
+            , 'is_male'
+            , 'phone'
+            , 'skin_color'
+        )
