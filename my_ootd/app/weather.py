747dc6ac442fc4845572a7b81b29984b85ceac02
dbckd999 <dbckd999@naver.com>
2023-04-15 19:07:22 +0900

    날씨 시험중

--- my_ootd/app/weather.py
+++ my_ootd/app/weather.py
@@ -9,16 +9,42 @@ base_date	발표일자
 base_time	발표시각
 nx	예보지점 X 좌표
 ny	예보지점 Y 좌표
-
 """
 
-serviceKey = 'om5Yy5MnYGk4Gvst9OAO%2BO2QB8RpxYEjR5KyIzp1S1T7Tai8%2FtF6ybNrxyWw8sHxPWxdApz1WlVeWCwMzZLiIQ%3D%3D'
+import requests
+from xml.etree import ElementTree
 
+# base url
 url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?'
 
-url += 'serviceKey=' + serviceKey + '&'
+# key
+serviceKey = 'om5Yy5MnYGk4Gvst9OAO%2BO2QB8RpxYEjR5KyIzp1S1T7Tai8%2FtF6ybNrxyWw8sHxPWxdApz1WlVeWCwMzZLiIQ%3D%3D'
+url += 'serviceKey=' + serviceKey
+
+numOfRows = 809
+url += '&numOfRows=' + str(numOfRows)
+
+pageNo = 1
+url += '&pageNo=' + str(pageNo)
+
+base_date = '20230413'
+url += '&base_date=' + base_date
+
+base_time = '0500'
+url += '&base_time=' + base_time
 
 # 대충 현재위치를 받는 기능
-nx = 0
-ny = 0
+nx = 89
+ny = 90
+url += '&nx=' + str(nx)
+url += '&ny=' + str(ny)
 
+# print(url)
+
+# cont = requests.get(url)
+# print(cont.text)
+
+with open('xml.txt') as file:
+    et = ElementTree.parse(file.read())
+    root = et.getroot()
+    print(root.tag)
