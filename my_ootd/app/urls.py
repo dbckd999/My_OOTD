775a95f83b59c7ae905ef8811c14821fa5ffd87f
dbckd999 <dbckd999@naver.com>
2023-04-28 16:54:30 +0900

    코드정리

--- my_ootd/app/urls.py
+++ my_ootd/app/urls.py
@@ -7,20 +7,10 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.root, name='root'),
-    # 회원가입 페이지
     path('sign-up', views.sign_up, name='sign_up'),
-    path('login', auth_views.LoginView.as_view(
-        template_name='app/login.html'), name='login'),
-    path('logout', auth_views.LogoutView.as_view(
-        template_name='app/main.html'), name='logout'),
+    path('login', auth_views.LoginView.as_view(template_name='app/login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(template_name='app/main.html'), name='logout'),
     path('mycloset', views.post_cloth, name='mycloset'),
     path('update_cloth', views.update_cloth, name='update_cloth'),
-
-
-
-
-
-
     path('create_user/', views.create_user, name='create_user'),
-
 ]
