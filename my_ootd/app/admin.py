57f59f8147addba3fd95d22d37b6864031d46918
dbckd999 <dbckd999@naver.com>
2023-05-07 18:46:23 +0900

    admin 페이지 추가

--- my_ootd/app/admin.py
+++ my_ootd/app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import SevUser
+from .models import SevUser, CodyLog
 
 # Register your models here.
 admin.site.register(SevUser)
+admin.site.register(CodyLog)
